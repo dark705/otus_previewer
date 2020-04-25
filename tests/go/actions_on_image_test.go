944c2ddd353a2999b29db320dc6f80b238df02df
dark705 <dark705r@mail.ru>
2020-04-25 19:11:37 +0300

    add integration tests: actions on image and supported image formats

--- tests/go/actions_on_image_test.go
+++ tests/go/actions_on_image_test.go
@@ -1,9 +1,88 @@
 package previewer_test
 
 import (
+	"fmt"
+	"image"
+	_ "image/gif"
+	_ "image/jpeg"
+	_ "image/png"
+	"net/http"
 	"testing"
 )
 
-//TODO release action on images tests
-func TestSimple(t *testing.T) {
+func TestResize(t *testing.T) {
+	width := 300
+	height := 200
+
+	resp, err := http.Get(fmt.Sprintf("http://previewer:8013/resize/%d/%d/nginx/test_image.jpg", width, height))
+	if err != nil {
+		t.Error("Fail on client get remote image", err)
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		t.Error(fmt.Sprintf("On resize existing image, Service return status code: %d, but expected code: %d",
+			resp.StatusCode, http.StatusOK))
+	}
+
+	img, _, err := image.DecodeConfig(resp.Body)
+	if err != nil {
+		t.Error("Fail on decode received image", err)
+	}
+
+	if img.Width != width || img.Height != height {
+		t.Error(fmt.Sprintf("On resize image to: %dx%d, Service return %dx%d dimensions",
+			width, height, img.Width, img.Height))
+	}
+}
+
+func TestFill(t *testing.T) {
+	width := 640
+	height := 480
+
+	resp, err := http.Get(fmt.Sprintf("http://previewer:8013/fill/%d/%d/nginx/test_image.jpg", width, height))
+	if err != nil {
+		t.Error("Fail on client get remote image", err)
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		t.Error(fmt.Sprintf("On fill existing image, Service return status code: %d, but expected code: %d",
+			resp.StatusCode, http.StatusOK))
+	}
+
+	img, _, err := image.DecodeConfig(resp.Body)
+	if err != nil {
+		t.Error("Fail on decode received image", err)
+	}
+
+	if img.Width != width || img.Height != height {
+		t.Error(fmt.Sprintf("On fill image to: %dx%d, Service return %dx%d dimensions",
+			width, height, img.Width, img.Height))
+	}
+}
+
+func TestFit(t *testing.T) {
+	width := 640
+	height := 200
+	fitWidth := 284
+	fitHeight := 200
+
+	resp, err := http.Get(fmt.Sprintf("http://previewer:8013/fit/%d/%d/nginx/test_image.jpg", width, height))
+	if err != nil {
+		t.Error("Fail on client get remote image", err)
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		t.Error(fmt.Sprintf("On fit existing image, Service return status code: %d, but expected code: %d",
+			resp.StatusCode, http.StatusOK))
+	}
+
+	img, _, err := image.DecodeConfig(resp.Body)
+	if err != nil {
+		t.Error("Fail on decode received image", err)
+	}
+
+	if img.Width != fitWidth || img.Height != fitHeight {
+		t.Error(fmt.Sprintf("On fit image to: %dx%d, Service return %dx%d dimensions",
+			fitWidth, fitHeight, img.Width, img.Height))
+	}
 }
---
+++ tests/go/supported_image_formats_test.go
@@ -0,0 +1,74 @@
+package previewer_test
+
+import (
+	"fmt"
+	"image"
+	_ "image/gif"
+	_ "image/jpeg"
+	_ "image/png"
+	"net/http"
+	"testing"
+)
+
+func TestFormatJpeg(t *testing.T) {
+	resp, err := http.Get("http://previewer:8013/resize/300/200/nginx/test_image.jpg")
+	if err != nil {
+		t.Error("Fail on client get remote image", err)
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		t.Error(fmt.Sprintf("On resize existing image, Service return status code: %d, but expected code: %d",
+			resp.StatusCode, http.StatusOK))
+	}
+
+	_, format, err := image.DecodeConfig(resp.Body)
+	if err != nil {
+		t.Error("Fail on decode received image", err)
+	}
+
+	if format != "jpeg" {
+		t.Error(fmt.Sprintf("On resize image format: jpeg, Service return format: %s", format))
+	}
+}
+
+func TestFormatPng(t *testing.T) {
+	resp, err := http.Get("http://previewer:8013/resize/300/200/nginx/test_image.png")
+	if err != nil {
+		t.Error("Fail on client get remote image", err)
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		t.Error(fmt.Sprintf("On resize existing image, Service return status code: %d, but expected code: %d",
+			resp.StatusCode, http.StatusOK))
+	}
+
+	_, format, err := image.DecodeConfig(resp.Body)
+	if err != nil {
+		t.Error("Fail on decode received image", err)
+	}
+
+	if format != "png" {
+		t.Error(fmt.Sprintf("On resize image format: png, Service return format: %s", format))
+	}
+}
+
+func TestFormatGif(t *testing.T) {
+	resp, err := http.Get("http://previewer:8013/resize/300/200/nginx/test_image.gif")
+	if err != nil {
+		t.Error("Fail on client get remote image", err)
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		t.Error(fmt.Sprintf("On resize existing image, Service return status code: %d, but expected code: %d",
+			resp.StatusCode, http.StatusOK))
+	}
+
+	_, format, err := image.DecodeConfig(resp.Body)
+	if err != nil {
+		t.Error("Fail on decode received image", err)
+	}
+
+	if format != "gif" {
+		t.Error(fmt.Sprintf("On resize image format: gif, Service return format: %s", format))
+	}
+}
