3a77de3ee1f045d9e983e53061ba6cae03b34fe7
Sergey Gulinov <sgulinov@stream.ru>
2020-04-28 01:35:29 +0300

    move handle steps to separate functions

--- internal/http/httpHandler.go
+++ internal/http/httpHandler.go
@@ -13,8 +13,6 @@ import (
 func handlerRequest(logger *logrus.Logger, imageDispatcher *dispatcher.ImageDispatcher, imageLimit int) http.HandlerFunc {
 	return func(responseWriter http.ResponseWriter, request *http.Request) {
 		responseWriter.Header().Set("Server", "Previewer")
-
-		//Check and parse request params
 		logger.Infoln(fmt.Sprintf("income request: %s %s %s", request.RemoteAddr, request.Method, request.URL))
 		parsedURL, err := ParseURL(request.URL)
 		if err != nil {
@@ -23,7 +21,6 @@ func handlerRequest(logger *logrus.Logger, imageDispatcher *dispatcher.ImageDisp
 			return
 		}
 
-		//Generate uniq id for request, witch will be used for save image
 		uniqID := GenUniqIDForURL(request.URL)
 		logger.Infoln(fmt.Sprintf("generate uniq reqId: %s for Url: %s", uniqID, request.URL.Path))
 
@@ -63,32 +60,48 @@ func handleNoCached(logger *logrus.Logger,
 	responseWriter http.ResponseWriter,
 	request *http.Request) {
 	logger.Infoln(fmt.Sprintf("image for uniq reqId: %s, not found in cache, need to dowload", uniqID))
-	response, err := makeRequest(parsedURL.RequestURL, request.Header, nil)
+	remoteResponse, err := makeRequest(parsedURL.RequestURL, request.Header, nil)
 	if err != nil {
 		http.Error(responseWriter, err.Error(), http.StatusBadGateway)
 		return
 	}
-	//If remote server response not StatusOk, proxy response to client with status, headers and body
-	if response.StatusCode != http.StatusOK {
-		bodyBytes, err := ioutil.ReadAll(response.Body)
-		_ = response.Body.Close()
-		if err != nil {
-			logger.Errorln(err)
-		}
-		logger.Warnln(fmt.Sprintf("remote server for url: %s return status: %d ", parsedURL.RequestURL, response.StatusCode))
-		for h, v := range response.Header {
-			responseWriter.Header().Set(h, v[0])
-		}
-		responseWriter.WriteHeader(response.StatusCode)
-		_, err = responseWriter.Write(bodyBytes)
-		if err != nil {
-			logger.Errorln(err)
-		}
+
+	if remoteResponse.StatusCode != http.StatusOK {
+		proxyErrorRemoteResponse(logger, remoteResponse, responseWriter, parsedURL)
 		return
 	}
 
-	//Status Ok, read response as image
-	im, err := image.ReadImageAsByte(response.Body, imageLimit)
+	handleRemoteResponse(logger, remoteResponse, responseWriter, imageDispatcher, uniqID, imageLimit, parsedURL)
+}
+
+func proxyErrorRemoteResponse(logger *logrus.Logger,
+	remoteResponse *http.Response,
+	responseWriter http.ResponseWriter,
+	parsedURL URLParams) {
+	bodyBytes, err := ioutil.ReadAll(remoteResponse.Body)
+	_ = remoteResponse.Body.Close()
+	if err != nil {
+		logger.Errorln(err)
+	}
+	logger.Warnf("remote server for url: %s return status: %d ", parsedURL.RequestURL, remoteResponse.StatusCode)
+	for h, v := range remoteResponse.Header {
+		responseWriter.Header().Set(h, v[0])
+	}
+	responseWriter.WriteHeader(remoteResponse.StatusCode)
+	_, err = responseWriter.Write(bodyBytes)
+	if err != nil {
+		logger.Errorln(err)
+	}
+}
+
+func handleRemoteResponse(logger *logrus.Logger,
+	response *http.Response,
+	responseWriter http.ResponseWriter,
+	imageDispatcher *dispatcher.ImageDispatcher,
+	uniqID string,
+	imageLimit int,
+	parsedURL URLParams) {
+	remoteImage, err := image.ReadImageAsByte(response.Body, imageLimit)
 	_ = response.Body.Close()
 	if err != nil {
 		logger.Warnln(err)
@@ -96,9 +109,8 @@ func handleNoCached(logger *logrus.Logger,
 		return
 	}
 
-	//Downloaded image as byte, make convert
 	logger.Infoln(fmt.Sprintf("success download image for uniq reqId: %s,", uniqID))
-	convertedImage, err := image.Resize(im, image.ResizeConfig{
+	convertedImage, err := image.Resize(remoteImage, image.ResizeConfig{
 		Action: parsedURL.Service,
 		Width:  parsedURL.Width,
 		Height: parsedURL.Height,
@@ -109,13 +121,11 @@ func handleNoCached(logger *logrus.Logger,
 		return
 	}
 
-	//send to client
 	_, err = responseWriter.Write(convertedImage)
 	if err != nil {
 		logger.Errorln(err)
 	}
 
-	//save to cache
 	err = imageDispatcher.Add(uniqID, convertedImage)
 	if err != nil {
 		logger.Errorln(err)
